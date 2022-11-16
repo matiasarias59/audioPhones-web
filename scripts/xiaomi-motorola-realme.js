08f21c73df1a60787d6f452084bf4cf369e80049
Matias Arias <matiasarias59@gmail.com>
2022-11-16 17:35:19 -0300

    page xiaomi

---
+++ scripts/xiaomi-motorola-realme.js
@@ -0,0 +1,33 @@
+import {getItemsJson, generateCatalogue, filtrarItems, createItemsCards, apiKey, range, url} from "../main.js"
+const catalogue = [];
+const itemList = document.getElementById("itemList");
+const itemList2 = document.getElementById("itemList2");
+const itemList3 = document.getElementById("itemList3");
+
+const filterFuncXiaomi= (item) => {
+    return (item["marca"]=="XIAOMI" && item["subFamilia"]=="NUEVO" )
+}
+
+const filterFuncRealme= (item) => {
+    return (item["marca"]=="REALME" && item["subFamilia"]=="NUEVO" )
+}
+
+const filterFuncMoto= (item) => {
+    return ((item["marca"]=="MOTOROLA" || item["marca"]=="ENERGIZER" )&& item["subFamilia"]=="NUEVO" )
+}
+
+
+
+const generatePage =async (asyncFunc, url) =>{
+    const items = await asyncFunc (url)
+    generateCatalogue(items, catalogue);
+    const arrXiaomi = await filtrarItems(catalogue, filterFuncXiaomi);
+    const arrRelame = await filtrarItems(catalogue, filterFuncRealme);
+    const arrMoto = await filtrarItems(catalogue, filterFuncMoto);
+
+    createItemsCards(arrXiaomi, itemList);
+    createItemsCards(arrRelame, itemList2);
+    createItemsCards(arrMoto, itemList3);
+}
+
+generatePage(getItemsJson, url+range+apiKey)
